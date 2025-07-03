a94de308b7acbbe61dcbf9fc41fbafbce4269eeb
limkang <dlarkdgus8@gmail.com>
2025-07-03 13:23:36 +0900

    Ganghyun
    TreeTexture

---
+++ Assets/Tree.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        Vector3 lookPos = target.transform.position;
+        lookPos.y = transform.position.y; // y�� ����(����)
+        transform.LookAt(lookPos);
+
+    }
+}
